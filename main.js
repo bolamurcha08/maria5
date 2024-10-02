cc30ac0912ed7731e687068232cac0c8beea9ac8
João Tasca <142262288+bolamurcha08@users.noreply.github.com>
2024-10-02 11:39:48 -0300

    fd

--- main.js
+++ main.js
@@ -15,4 +15,4 @@
 //  console.log (media (4, 5, 6))
  
 
-let estados=[ 'PR','RJ','AC','AL','AP','AM','BA','CE','DF', 'ES','GO','MA','MT','MS','MG','PA','PB','PE','PI','RN', 'RS','RO','RR','SC','SP','SE','TO']
+let estados =[ 'PR','RJ','AC','AL','AP','AM','BA','CE','DF', 'ES','GO','MA','MT','MS','MG','PA','PB','PE','PI','RN', 'RS','RO','RR','SC','SP','SE','TO']
