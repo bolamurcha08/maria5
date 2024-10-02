30619cb88bbb9d5a4e25b88a5c97ec178af7460f
João Tasca <142262288+bolamurcha08@users.noreply.github.com>
2024-10-02 11:36:52 -0300

    r

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
- alert ('joao e maria')
- function soma (numero1 , numero2) {
-    return numero1+numero2;
- }
- console.log (soma (5,2));
- function calcularAreaQuadrado (lado){
-    return lado*lado;
- }
- console.log (calcularAreaQuadrado (5));
- let numero=prompt('qual sua idade?');
- document.write (qual sua idade?)
- let media= function (x,y,z){
-    return x+y+z/3;
- }
- console.log (media (4, 5, 6))
+//  alert ('joao e maria')
+//  function soma (numero1 , numero2) {
+//     return numero1+numero2;
+//  }
+//  console.log (soma (5,2));
+//  function calcularAreaQuadrado (lado){
+//     return lado*lado;
+//  }
+//  console.log (calcularAreaQuadrado (5));
+//  let numero=prompt('qual sua idade?');
+//  document.write (qual sua idade?)
+//  let media= function (x,y,z){
+//     return x+y+z/3;
+//  }
+//  console.log (media (4, 5, 6))
  
+
+let estados=[ 'PR','RJ','AC','AL','AP','AM','BA','CE','DF', 'ES','GO','MA','MT','MS','MG','PA','PB','PE','PI','RN', 'RS','RO','RR','SC','SP','SE','TO']
+estados.
